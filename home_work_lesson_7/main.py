bfdc92cf771f38c11bcfcb78e23d20236a6427fe
MutherGun <kosarekjirka@seznam.cz>
2025-04-01 22:36:00 +0200

    Oprava lekce

--- home_work_lesson_7/main.py
+++ home_work_lesson_7/main.py
@@ -1,6 +1,6 @@
 from home_work_lesson_7.funciones.name_check import is_name_valid
 from home_work_lesson_7.funciones.age_check import is_adult
-from home_work_lesson_7.users.users import user
+from home_work_lesson_7.users.users import users
 
 while True:
     name = input("Enter user name:")
@@ -27,8 +27,40 @@ email = input("Enter email:")
 
 if is_name_valid(name) and is_adult(age):
     new_user = {"name": name, "age": age, "email": email}
-    user.append(new_user)
+    users.append(new_user)
     print("Registration succes!")
 
-for u in user:
-    print(list(u.values()))
+def create_user(username, age, email):
+    if not is_name_valid(username):
+        return{"success": False, "error": "User name must have atleast four characters."}
+    if not is_adult(age):
+        return{"success": False, "error": "User must be at least 18."}
+    return{
+        "success":True, "user":
+            {"username": username, "age": age, "email": email}}
+
+def print_user_info(user_result):
+    if user_result["success"]:
+        user = user_result["user"]
+        print(f"user: {user['username']}, age: {user['age']}, email: {user['email']}")
+    else:
+        print(f"Error: {user_result['error']}")
+
+user_result = create_user(name, age, email)
+
+if user_result["success"]:
+    users.append(user_result["user"])
+    print("Registration success!")
+
+user_list = [
+            create_user("Jana", 25, "jana@jana.com"),
+            create_user("AB", 20, "ab@ab.com"),
+            create_user("Pepa", 17, "pepa@pepa.com"),
+            create_user("Veronika", 30, "veronika@veronika.com")]
+
+for user in user_list:
+    print_user_info(user)
+
+print("\nAll registered users:")
+for u in users:
+     print(list(u.values()))
--- home_work_lesson_7/users/users.py
+++ home_work_lesson_7/users/users.py
@@ -1,4 +1,4 @@
-user = [
+users = [
     {"name": "Frank", "age": 25, "email": "frank@frank.com"},
     {"name": "George", "age": 35, "email": "george@george.com"},
     {"name": "Luci", "age": 28, "email": "luci@luci.com"},
